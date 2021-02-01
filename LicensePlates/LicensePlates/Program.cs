1be04c895a101f95e59880385781b504535509d8
rodman864 <59933094+rodman864@users.noreply.github.com>
2021-02-01 12:34:14 -0500

    Add files via upload

---
+++ LicensePlates/LicensePlates/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LicensePlates
+{
+    public class MyDMV
+    {
+
+        static void Main()
+        {
+
+
+            DMV myDMV = new DMV();
+
+            TagInfo myTag = new TagInfo();
+
+            myTag.TagNumber = "4503";
+            myTag.TagState = "South Carolina";
+            myDMV.AddTag(myTag);
+            
+
+            TagInfo myTag2 = new TagInfo();
+            myTag2.TagNumber = "4504";
+            myTag2.TagState = "North Carolina";
+            myDMV.AddTag(myTag);
+
+
+            myDMV.ShowTags();
+
+
+        }
+    }
+}
+public class DMV
+{
+   
+    public HashSet<TagInfo> mySet = new HashSet<TagInfo>();
+
+    public void AddTag(TagInfo TagNumber)
+    {
+        bool result = mySet.Add(TagNumber);
+       if (result==false)
+        {
+            Console.WriteLine("Tag number already exists");
+        }
+        else
+        {
+            Console.WriteLine("Please register this tag to the system");
+        }
+    }
+   public void ShowTags()
+    {
+        Console.WriteLine("You have this many tags: " +
+                  mySet.Count);
+    }
+}
+public class TagInfo
+{
+    public string TagNumber { get; set; }
+    public string TagState { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        TagInfo newClass = obj as TagInfo;
+
+        return this.TagNumber.Equals(newClass.TagNumber);
+    }
+
+    public override int GetHashCode()
+    {
+        return this.TagNumber.GetHashCode();
+    }
+
+}
